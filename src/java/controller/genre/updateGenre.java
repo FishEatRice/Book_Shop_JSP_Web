169dfc9f931dfd6f3c0b50fd0bd8f1c37e6baa4a
wenwei-rgb <soowenwei2813@gmail.com>
2025-04-14 12:59:19 +0800

    remove unused library

--- src/java/controller/genre/updateGenre.java
+++ src/java/controller/genre/updateGenre.java
@@ -5,13 +5,11 @@
 package controller.genre;
 
 import jakarta.persistence.*;
-import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import static java.lang.System.out;
 import model.genre.Genre;
 
 public class updateGenre extends HttpServlet {
